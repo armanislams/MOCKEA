39564a9f62067230294410f8253cd253f5c9d5c3
ArmanIslamTC <armanislam.ais@gmail.com>
2026-05-19 19:41:37 +0800

    change google logo svg to react icon

--- frontend/src/components/Auth/SocialLoginButton.jsx
+++ frontend/src/components/Auth/SocialLoginButton.jsx
@@ -4,6 +4,7 @@ import useAxios from "../../hooks/useAxios";
 import { toast } from "react-toastify";
 import { useNavigate, useLocation } from "react-router";
 import { motion } from "framer-motion";
+import { FaGoogle } from "react-icons/fa6";
 
 const SocialLoginButton = ({ onSuccess }) => {
   const { signInGoogle, setLoading } = useAuth();
@@ -79,30 +80,8 @@ const SocialLoginButton = ({ onSuccess }) => {
           </>
         ) : (
           <>
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5 shrink-0"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 48 48"
-            >
-              <path
-                fill="#EA4335"
-                d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
-              />
-              <path
-                fill="#4285F4"
-                d="M46.5 24c0-1.61-.15-3.16-.42-4.69H24v8.89h12.66c-.55 2.87-2.17 5.3-4.61 6.94l7.16 5.55C43.34 36.5 46.5 30.82 46.5 24z"
-              />
-              <path
-                fill="#FBBC05"
-                d="M10.54 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.98-6.19z"
-              />
-              <path
-                fill="#34A853"
-                d="M24 38.5c-6.26 0-11.57-4.22-13.46-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48c6.48 0 11.93-2.13 15.89-5.81l-7.16-5.55c-2.44 1.64-5.57 2.87-8.73 2.87z"
-              />
-            </svg>
-            <span>Continue with Google</span>
+          <FaGoogle className="" size={20} />
+          <span>Continue with Google</span>
           </>
         )}
       </motion.button>
